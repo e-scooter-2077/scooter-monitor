26933d01782f67589e33d3a01e9ec2c17dfedba2
ldeluigi <lucadelu97@gmail.com>
2022-01-13 19:04:00 +0100

    Add unit tests

--- EScooter.Monitor/Monitor.cs
+++ EScooter.Monitor/Monitor.cs
@@ -3,6 +3,7 @@ using EasyDesk.CleanArchitecture.Application.Events.ExternalEvents;
 using EasyDesk.CleanArchitecture.Infrastructure.Events.ServiceBus;
 using EasyDesk.CleanArchitecture.Infrastructure.Json;
 using EasyDesk.CleanArchitecture.Infrastructure.Time;
+using EasyDesk.Tools.Options;
 using EScooter.Monitor.Web.DTO;
 using Microsoft.Azure.Devices.Shared;
 using Microsoft.Azure.Functions.Worker;
@@ -12,6 +13,7 @@ using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using static EasyDesk.Tools.Options.OptionImports;
 
 namespace EScooter.Monitor
 {
@@ -29,15 +31,8 @@ namespace EScooter.Monitor
             var logger = context.GetLogger(typeof(Monitor).Name);
             logger.LogInformation($"C# ServiceBus topic trigger function processed message: {mySbMsg}");
             userProperties.TryGetValue("deviceId", out object scooterId);
-
-            var scooterDeviceTwin = JsonConvert.DeserializeObject<Twin>(mySbMsg, new TwinJsonConverter());
-            var reportedProps = scooterDeviceTwin.Properties.Reported;
-            var reportedPropsObj = JsonConvert.DeserializeObject<ReportedPropertiesDTO>(reportedProps.ToJson(), serializerSettings);
-            if (reportedPropsObj is { Locked: null, MaxSpeed: null, Standby: null, UpdateFrequency: null })
-            {
-                return;
-            }
             var id = scooterId.ToString();
+            var res = CreateStatusEventFromTriggerMessage(id, mySbMsg, serializerSettings);
 
             var connectionString = Environment.GetEnvironmentVariable(ServiceBusVariableName);
             var serviceBusClient = new ServiceBusClient(connectionString);
@@ -45,14 +40,27 @@ namespace EScooter.Monitor
             var eventBusPublisher = new AzureServiceBusPublisher(serviceBusClient, topicDescriptor);
             var serializer = new NewtonsoftJsonSerializer(serializerSettings);
             var externalEventsPublisher = new ExternalEventPublisher(eventBusPublisher, new MachineDateTime(), serializer);
+
+            await res.IfPresentAsync(async x => await externalEventsPublisher.Publish(x));
+            return;
+        }
+
+        public static Option<ScooterStatusChangedDTO> CreateStatusEventFromTriggerMessage(string id, string msg, JsonSerializerSettings serializerSettings)
+        {
+            var scooterDeviceTwin = JsonConvert.DeserializeObject<Twin>(msg, new TwinJsonConverter());
+            var reportedProps = scooterDeviceTwin.Properties.Reported;
+            var reportedPropsObj = JsonConvert.DeserializeObject<ReportedPropertiesDTO>(reportedProps.ToJson(), serializerSettings);
+            if (reportedPropsObj is { Locked: null, MaxSpeed: null, Standby: null, UpdateFrequency: null })
+            {
+                return None;
+            }
             var scooterStatus = new ScooterStatusChangedDTO(
                 Id: id,
                 Locked: reportedPropsObj.Locked,
                 UpdateFrequency: reportedPropsObj.UpdateFrequency,
                 MaxSpeed: reportedPropsObj.MaxSpeed,
                 Standby: reportedPropsObj.Standby);
-            await externalEventsPublisher.Publish(scooterStatus);
-            return;
+            return Some(scooterStatus);
         }
     }
 }
---
+++ test/EScooter.Monitor.UnitTests/CreateStatusEventFromTriggerMessageTest.cs
@@ -0,0 +1,74 @@
+﻿using EasyDesk.Tools.Options;
+using EScooter.Monitor.Web.DTO;
+using Microsoft.Azure.Devices.Shared;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace EScooter.Monitor.UnitTests
+{
+    public class CreateStatusEventFromTriggerMessageTest
+    {
+        private Twin Twin()
+        {
+            var t = new Twin();
+            t.DeviceId = null;
+            t.Properties.Reported = new TwinCollection();
+            t.Properties.Reported["Locked"] = null;
+            t.Properties.Reported["MaxSpeed"] = null;
+            t.Properties.Reported["Standby"] = null;
+            t.Properties.Reported["UpdateFrequency"] = null;
+
+            return t;
+        }
+
+        private bool _locked = true;
+        private double _maxSpeed = 10;
+        private bool _standby = false;
+        private string _updateFrequency = "00:00:30";
+
+        private Twin CompleteTwin()
+        {
+            var t = new Twin();
+            t.DeviceId = "___asddsas_";
+            t.Properties.Reported["Locked"] = _locked;
+            t.Properties.Reported["MaxSpeed"] = _maxSpeed;
+            t.Properties.Reported["Standby"] = _standby;
+            t.Properties.Reported["UpdateFrequency"] = _updateFrequency;
+            return t;
+        }
+
+        private JsonSerializerSettings _serializerSettings = new JsonSerializerSettings()
+        {
+            ContractResolver = new CamelCasePropertyNamesContractResolver()
+        };
+
+        [Fact]
+        public void ShouldReturnNondeWithNullFields()
+        {
+            var t = Twin();
+            Monitor.CreateStatusEventFromTriggerMessage(t.DeviceId, JsonConvert.SerializeObject(t, new TwinJsonConverter()), _serializerSettings)
+                .ShouldBe(OptionImports.None);
+        }
+
+        [Fact]
+        public void ShouldReturnCorrectFieldsWithCorrectInput()
+        {
+            var twin = CompleteTwin();
+            Monitor.CreateStatusEventFromTriggerMessage(twin.DeviceId, JsonConvert.SerializeObject(twin, new TwinJsonConverter()), _serializerSettings)
+                .ShouldBe(new ScooterStatusChangedDTO(
+                    Id: twin.DeviceId,
+                    Locked: _locked,
+                    UpdateFrequency: _updateFrequency,
+                    Standby: _standby,
+                    MaxSpeed: _maxSpeed
+                    ));
+        }
+    }
+}
